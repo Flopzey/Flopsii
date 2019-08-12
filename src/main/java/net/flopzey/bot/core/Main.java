de8b498502134281f02bdc98a2d7f4d3618fa1a7
Flopzey <git@flopzey.net>
2019-08-12 20:34:23 +0200

    Changes in package structure, .gitignore and pom.xml

--- src/main/java/net/flopzey/bot/core/Main.java
+++ src/main/java/net/flopzey/bot/core/Main.java
@@ -1,4 +1,4 @@
-package core;
+package net.flopzey.bot.core;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -6,13 +6,20 @@ import org.ini4j.Ini;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String args[]) {
 
         try {
-            JDA jda = new JDABuilder("TOKEN").build();
+            Ini ini = new Ini(new File("config.ini"));
+
+            // todo - Constants
+            JDA jda = new JDABuilder(ini.get("BotConfig", "Token")).build();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         } catch (LoginException ex) {
             ex.printStackTrace();
         }
