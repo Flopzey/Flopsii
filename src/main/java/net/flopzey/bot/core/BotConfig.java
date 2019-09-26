6ebd7d8e914db0e63fde8daf114d293c7432073c
Flopzey <git@flopzey.net>
2019-09-26 21:19:50 +0200

    BotConfig is now used to receive the api-token from config.ini

--- src/main/java/net/flopzey/bot/core/BotConfig.java
+++ src/main/java/net/flopzey/bot/core/BotConfig.java
@@ -1,14 +1,38 @@
 package net.flopzey.bot.core;
 
+import org.ini4j.Ini;
+import org.ini4j.Profile;
+
+import java.io.File;
+import java.io.IOException;
+
 public class BotConfig {
 
-    public BotConfig() {
+    private static final String SECTION_BOT_CONFIG = "BotConfig";
 
+    private static Profile.Section botConfig;
+
+    static {
+
+        try {
+            Ini ini = new Ini(new File("config.ini"));
+            botConfig = ini.get(SECTION_BOT_CONFIG);
+        } catch (IOException ex) {
+            // todo - Logger
+        }
 
     }
 
+    protected static String getBotToken() {
+        return botConfig.get("Token");
+    }
+
     public static String getCommandPrefix() {
-        return "!"; // temp
+        return botConfig.get("Command_Prefix");
+    }
+
+    public static String getBotOwnerID() {
+        return botConfig.get("Bot_Owner");
     }
 
 }
