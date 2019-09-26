2ef32d9c7ff4d79b46d645ad8bd12a8f1f5ab642
Flopzey <git@flopzey.net>
2019-09-26 21:20:31 +0200

    Added functional commands shutdown, clear and random; added empty commands help and whois

---
+++ src/main/java/net/flopzey/bot/commands/categories/admin/ClearCommand.java
@@ -0,0 +1,68 @@
+package net.flopzey.bot.commands.categories.admin;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.flopzey.bot.commands.BaseCommand;
+import net.flopzey.bot.commands.Command;
+import net.flopzey.bot.utils.BotUtils;
+import net.flopzey.bot.utils.MessageUtils;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Command(
+        aliases = {"clear", "c"},
+        description = "very nice description"
+)
+public class ClearCommand extends BaseCommand {
+
+    @Override
+    public boolean preExecute(MessageReceivedEvent event) {
+        return event.getMember().hasPermission(Permission.ADMINISTRATOR);
+    }
+
+    @Override
+    public void execute(String[] args, MessageReceivedEvent event) {
+
+        int count = (args.length > 0) ? BotUtils.parseInt(args[0]) : 0;
+
+        // delete initial message
+        event.getMessage().delete().queue();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            // todo - Logger
+        }
+
+        MessageHistory history = event.getTextChannel().getHistory();
+        List<Message> messageHistory;
+
+        if (count > 0 && count < 100) {
+            messageHistory = history.retrievePast(count).complete();
+
+            if (messageHistory.size() == 1) {
+                messageHistory.get(0).delete().queue();
+            } else {
+                event.getTextChannel().deleteMessages(messageHistory).queue();
+            }
+
+            Message message = event.getTextChannel().sendMessage(
+                    MessageUtils.getSuccessMessage("Deleted messages: " + count))
+                    .complete();
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    message.delete().queue();
+                }
+            }, 3000);
+
+        }
+
+        // todo - handling of wrong usage
+
+    }
+
+}
